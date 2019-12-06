b4952820259c8b2d5d47eb2373a043b3a5a2ad11
Alex Willisson <alex@exaptationllc.com>
2019-12-06 12:59:21 -0500

    added uuids and a keyed product list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import uuid from 'react-uuid'
 
-class Item extends React.Component {
+class Product extends React.Component {
+    constructor(props) {
+	super(props);
+
+	this.state = {
+	    key: this.props.productDetails.key,
+	    name: this.props.productDetails.name,
+	    price: this.props.productDetails.price,
+	    salePrice: this.props.productDetails.salePrice,
+	    maxQuantity: this.props.productDetails.maxQuantity,
+	    addToCart: this.props.addToCart
+	}
+    }
+
     render() {
 	return (
 	    <tr>
-	    <td>{this.props.name}</td>
-	    <td>16.99</td>
-	    <td>None</td>
-	    <td>None</td>
-	    <td><button onClick={this.props.addToCart}>Add To Cart</button></td>
+	    <td>{this.state.name}</td>
+	    <td>{this.state.price}</td>
+	    <td>{this.state.salePrice}</td>
+	    <td>{this.state.maxQuantity}</td>
+	    <td><button onClick={this.state.addToCart}>Add To Cart</button></td>
 	    </tr>
 	);
     }
@@ -19,21 +33,80 @@ class Item extends React.Component {
 class Store extends React.Component {
     constructor(props) {
 	super(props);
+
+	const initialProducts = [
+	    {
+		key: uuid(),
+		name: "Basketball",
+		price: 16.99,
+		salePrice: null,
+		maxQuantity: null
+	    },
+	    {
+		key: uuid(),
+		name: "Baseball",
+		price: 6.49,
+		salePrice: 5.99,
+		maxQuantity: null
+	    },
+	    {
+		key: uuid(),
+		name: "Football",
+		price: 20.00,
+		salePrice: 10.00,
+		maxQuantity: 4
+	    },
+	    {
+		key: uuid(),
+		name: "Basketball",
+		price: 16.99,
+		salePrice: 12.99,
+		maxQuantity: 1
+	    },
+	    {
+		key: uuid(),
+		name: "Lacrosse Stick",
+		price: 39.99,
+		salePrice: null,
+		maxQuantity: null
+	    }
+	]
+
+	let keyedProductList = {}
+
+	initialProducts.forEach(product => keyedProductList[product.key] = product);
+
 	this.state = {
-	    productList: ["Basketball", "Baseball", "Football", "Basketball", "Lacrosse Stick"],
+	    productList: initialProducts,
+	    keyedProductList: Object.assign({}, keyedProductList),
 	    shoppingCart: []
 	};
+
+	console.log("Hi Zak/AbbVie,\n\nTo be honest I just learned React. Decided to put my time into building the challenge since this error wasn't clear what it meant and I didn't want to burn all my time on it");
     }
 
     addProduct = () => {
-	const productList = this.state.productList.slice();
-	productList.push("Foobar");
-	this.setState({productList: productList});
+	// Yes this is inefficient. Going for immutability over runtime here
+	let productList = this.state.productList.slice();
+	let keyedProductList = Object.assign({}, this.state.keyedProductList);
+
+	const newProduct = {
+	    key: uuid(),
+	    name: "Sparx Skate Sharpener",
+	    price: 399.99,
+	    salePrice: null,
+	    maxQuantity: null
+	};
+	productList.push(newProduct);
+	keyedProductList[newProduct.key] = newProduct
+
+	this.setState({productList: productList.slice(),
+		       keyedProductList: Object.assign({}, keyedProductList)});
     }
 
-    addToCart = () => {
+    addToCart = (key) => {
 	const shoppingCart = this.state.shoppingCart.slice();
-	shoppingCart.push("Foobar");
+	shoppingCart.push(this.state.keyedProductList[key]);
 	this.setState({shoppingCart: shoppingCart});
     }
 
@@ -42,10 +115,12 @@ class Store extends React.Component {
 	let idx;
 
 	for (idx = 0; idx < this.state.productList.length; idx++) {
+	    let product = this.state.productList[idx]
+
 	    products.push(
-		<Item
-		name={this.state.productList[idx]}
-		addToCart={() => this.addToCart()}
+		<Product
+		productDetails={product}
+		addToCart={() => this.addToCart(product.key)}
 		/>);
 	}
 
@@ -58,9 +133,9 @@ class Store extends React.Component {
 
 	for (idx = 0; idx < this.state.shoppingCart.length; idx++) {
 	    products.push(
-		<Item
-		name={this.state.shoppingCart[idx]}
-		addToCart={() => this.addToCart()}
+		<Product
+		productDetails={this.state.shoppingCart[idx]}
+		delFromCart={() => this.delFromCart()}
 		/>);
 	}
 
